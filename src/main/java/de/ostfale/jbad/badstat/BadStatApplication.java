152956b74332d54d9cbea4c603055c96a7938178
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2024-12-02 18:27:55 +0100

    Initial Commit

---
+++ src/main/java/de/ostfale/jbad/badstat/BadStatApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.jbad.badstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadStatApplication.class, args);
+    }
+
+}
